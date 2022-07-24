a222f4073cae61e746bb69499f7ad07883f025ca
Bee Clark <mrhumanman2@gmail.com>
2022-07-23 21:38:31 -0400

    Start working on CLI

--- cli/src/main.rs
+++ cli/src/main.rs
@@ -1,3 +1,32 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::{Context, Result};
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[clap(
+    author = "Bee Clark",
+    version = "0.1.0",
+    about = "CLI for a file tagging app called Trapezoid"
+)]
+struct Cli {
+    #[clap(subcommand)]
+    subcommand: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Tag {},
+}
+
+fn main() -> Result<()> {
+    let args = Cli::parse();
+    let content = std::fs::read_to_string(&args.path)
+        .with_context(|| format!("could not read file {:#?}", args.path.as_path()))?;
+
+    for line in content.lines() {
+        if line.contains(&args.pattern) {
+            println!("{}", line);
+        }
+    }
+
+    Ok(())
 }
